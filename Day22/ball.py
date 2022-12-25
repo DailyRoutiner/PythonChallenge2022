5622646d8563dbb06af925624e48381a62e1c287
Jun <ponmons@naver.com>
2022-12-25 08:38:10 -0500

    Make a ball and paddles to move by key

---
+++ Day22/ball.py
@@ -0,0 +1,16 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.color("white")
+        self.goto(0, 0)
+
+    def move(self):
+        new_x = self.xcor() + 5
+        new_y = self.ycor() + 5
+        self.goto(new_x, new_y)
+
