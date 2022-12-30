84f8ff2f5c0690b362e219361b6dfde380131e38
Jun <ponmons@naver.com>
2022-12-29 22:56:30 -0500

    Day 24 create local file and read file

--- Day24/scoreboard.py
+++ Day24/scoreboard.py
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +16,29 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.high_score = self.open_data()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            self.save_data(self.high_score)
+        self.score = 0
+        self.update_scoreboard()
+
+    def save_data(self, score):
+        with open("data.txt", mode="w") as file:
+            file.write(str(score))
+
+    def open_data(self):
+        with open("data.txt") as file:
+            return int(file.read())
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
