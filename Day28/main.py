29ae11f294fd2df81a21480fec7f3553b4477672
Jun <ponmons@naver.com>
2023-01-16 23:47:42 +0900

    Day28 UI, countdown

--- Day28/main.py
+++ Day28/main.py
@@ -10,18 +10,31 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5*60)
+    global reps
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    # if it's the 1st/3rd/5th/7th
+    if reps % 2 == 1:
+        count_down(work_sec)
+    # if it's the 8th
+    count_down(long_break_sec)
+    # if it's the 2nd/4th/6th
+    count_down(short_break_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
 
     count_min = math.floor(count/60)
     count_sec = count % 60
+    if count_sec in range(0, 10):
+        count_sec = f"0{count_sec}"
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
