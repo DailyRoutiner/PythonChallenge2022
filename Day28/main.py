fa518d2ffa319a0b7f76ef51ad12958597e0c197
Jun <ponmons@naver.com>
2023-01-17 15:42:32 +0900

    Day28 Reset Timer, add session check marks

--- Day28/main.py
+++ Day28/main.py
@@ -7,26 +7,43 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
 LONG_BREAK_MIN = 20
 reps = 0
+my_timer = None
 # ---------------------------- TIMER RESET ------------------------------- # 
+
+def reset_time():
+    window.after_cancel(my_timer)
+    # 00:00
+    canvas.itemconfig(timer_text, text="00:00")
+    # title label
+    timer.config(text="Timer", fg=GREEN)
+    # reset check_marks
+    check_marks.config(text="")
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
     global reps
-    work_sec = WORK_MIN * 60
-    short_break_sec = SHORT_BREAK_MIN * 60
+    reps += 1
+    work_sec = WORK_MIN * 2
+    short_break_sec = SHORT_BREAK_MIN * 2
     long_break_sec = LONG_BREAK_MIN * 60
 
-    # if it's the 1st/3rd/5th/7th
-    if reps % 2 == 1:
-        count_down(work_sec)
     # if it's the 8th
-    count_down(long_break_sec)
-    # if it's the 2nd/4th/6th
-    count_down(short_break_sec)
+    if reps % 8 == 0:
+        timer.config(text="Break", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer.config(text="Break", fg=PINK)
+        # if it's the 2nd/4th/6th
+        count_down(short_break_sec)
+    else:
+        timer.config(text="Work", fg=GREEN)
+        # if it's the 1st/3rd/5th/7th
+        count_down(work_sec)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
@@ -38,7 +55,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count-1)
+        global my_timer
+        my_timer = window.after(1000, count_down, count-1)
+    else:
+        start_timer()
+        marks = ""
+        session = math.floor(reps/2)
+        for _ in range(session):
+            marks += "✔"
+        check_marks.config(text=marks)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -58,12 +83,12 @@ canvas.grid(column=1, row=1)
 timer = Label(text="Timer", font=(FONT_NAME, 45, "normal"), fg=GREEN, bg=YELLOW)
 timer.grid(column=1, row=0)
 start = Button(text="Start", highlightbackground=YELLOW, command=start_timer)
-reset = Button(text="Reset", highlightbackground=YELLOW)
+reset = Button(text="Reset", highlightbackground=YELLOW, command=reset_time)
 start.grid(column=0, row=2)
 reset.grid(column=2, row=2)
 
 # copy-paste checkmark
-check_marks = Label(text="✔", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
 
 
