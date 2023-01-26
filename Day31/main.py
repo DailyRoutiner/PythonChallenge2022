ac38fc38db2e2d44c1a3635c3c9026f6bd4f0405
jun <ponmons@naver.com>
2023-01-26 16:05:42 +0900

    Day31 flip card

--- Day31/main.py
+++ Day31/main.py
@@ -3,6 +3,7 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
 
 # shuffle word
 df = pandas.read_csv("./data/french_words.csv")
@@ -10,19 +11,32 @@ to_learn = df.to_dict(orient="records")# very important to get records
 
 
 def next_card():
+    global current_card, flip_time
+    window.after_cancel(flip_time)
     current_card = random.choice(to_learn)
-    board_canvas.itemconfig(card_title, text="French")
-    board_canvas.itemconfig(card_word, text=current_card["French"])
+    board_canvas.itemconfig(card_title, text="French", fill="black")
+    board_canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    board_canvas.itemconfig(board, image=card_front)
+    flip_time = window.after(3000, flip_card)
+
+def flip_card():
+    board_canvas.itemconfig(board, image=card_back)
+    board_canvas.itemconfig(card_title, text="English", fill="white")
+    board_canvas.itemconfig(card_word, text=current_card["English"], fill="white")
+
+
 
 # UI
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_time = window.after(3000, flip_card)
 
 board_canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
-board_image = PhotoImage(file="./images/card_front.png")
-board_canvas.create_image(400, 263, image=board_image)
+card_front = PhotoImage(file="./images/card_front.png")
+card_back = PhotoImage(file="./images/card_back.png")
+board = board_canvas.create_image(400, 263, image=card_front)
 
 card_title = board_canvas.create_text(400, 150, text="", fill="black", font=("Courier", 40, "italic"))
 card_word = board_canvas.create_text(400, 263, text="", fill="black", font=("Courier", 60, "bold"))
